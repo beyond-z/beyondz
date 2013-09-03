138efdaaa859be544b01dafdee5c6b61bb72bff3
Miki Heller <miki.heller@gmail.com>
2013-09-02 20:39:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beyondz::Application.config.secret_token = '85550d2e18cc8a64eb5a1cf93065353818173ee9c36db8b2f86dfcbcff44894ca18500bb5dc04d348a70db03707ccbd6d36619f7a0e2a3b9c452540baedb7f3c'
